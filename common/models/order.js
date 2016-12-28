f9a2b2cc5859f862b22487f8dace44e2d61f4ca4
Nick <rogersnm@hotmail.co.uk>
2016-12-28 22:17:56 +0000

    Implement very simple charge endpoint

--- common/models/order.js
+++ common/models/order.js
@@ -102,6 +102,35 @@ module.exports = function (Order) {
     returns: {arg: 'order', type: 'object'},
     http: {path: '/make', verb: 'post'}
   })
+
+    /**
+     * Process a name change charge
+     * @param nameChange
+     * @param req
+     * @returns {Promise}
+     */
+    Order.processNameChangeFee = (nameChange, req) => {
+      if (!req.accessToken.userId && req.accessToken.userId !== 0) {
+        throw new Error('Authentication failed.');
+      }
+
+      return stripe.charges.create({
+        amount: Math.round(order.total * 100),
+        currency: 'GBP',
+        source: order.paymentToken,
+        description: 'Churchill Spring Ball Tickets',
+        statement_descriptor: 'Chu Ball Tickets'
+      }).then(() => Order.create(order));
+    };
+
+    Order.remoteMethod('processNameChangeFee', {
+      accepts: [
+        {arg: 'order', type: 'object', http: {source: 'body'}, required: true},
+        {arg: 'req', type: 'object', http: {source: 'req'}},
+      ],
+      returns: {arg: 'order', type: 'object'},
+      http: {path: '/namechangefee', verb: 'post'}
+    })
 };
 
 /**
